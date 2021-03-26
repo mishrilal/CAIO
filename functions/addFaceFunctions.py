e85134d2c2436539a0c4d07f7635f2a292371837
mishrilal <mishrilalchhaparia@gmail.com>
2021-03-27 01:15:31 +0530

    Unique image name

--- functions/addFaceFunctions.py
+++ functions/addFaceFunctions.py
@@ -1,14 +1,6 @@
-import os
-import sys
-import time
-
 import cv2
-import numpy as np
 
-from PyQt5.QtWidgets import *
-from PyQt5.QtMultimedia import *
-
-from PySide2.QtCore import QObject, Slot, Signal
+from PySide2.QtCore import QObject, Slot, Signal, QSettings
 
 # Load HAAR face classifier
 face_classifier = cv2.CascadeClassifier('Haarcascades/haarcascade_frontalface_default.xml')
@@ -34,11 +26,22 @@ def face_extractor(img):
 
 class AddFace(QObject):
     setCaptureDetails = Signal(str)
+    person = 0
+
     # logic = 0
     #
-    # # constructor
-    # def __init__(self):
-    #     super().__init__()
+    # constructor
+    def __init__(self):
+        super().__init__()
+        self.settings = QSettings('CAIO', 'Preferences')
+        self.person = self.settings.value('person')
+        print(self.settings.value('person'))
+
+        if self.person is None:
+            self.settings.setValue('person', 1)
+            self.person = self.settings.value('person')
+            print(self.person)
+
     #
     #     # getting available cameras
     #     self.available_cameras = QCameraInfo.availableCameras()
@@ -113,26 +116,22 @@ class AddFace(QObject):
                 face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
 
                 # Save file in specified directory with unique name
-                timestamp = time.strftime("%d-%b-%Y-%H_%M_%S")
-                imgName = 'img_'+timestamp+'_'+str(count)
+                imgName = 'img_person' + str(self.person) + '_' + str(count)
                 file_name_path = 'images/captured/' + imgName + '.jpg'
                 cv2.imwrite(file_name_path, face)
 
                 # Put count on images and display live count
                 cv2.putText(face, str(count), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
                 # cv2.imshow('Face Cropper', face)
-
             else:
                 print("Face not found")
                 pass
 
             if cv2.waitKey(1) == 13 or count == 100:  # 13 is the Enter Key
                 break
-
+        self.person += 1
+        self.settings.setValue('person', self.person)
         cap.release()
         cv2.destroyAllWindows()
         print("Collecting Samples Complete")
         self.setCaptureDetails.emit("Collected Samples Successfully")
-
-
-
